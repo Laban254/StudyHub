b507b295d701c1f1ae9e5dd4fac74db53bae5cfd
laban254 <labanrotich6544@gmail.com>
2024-04-20 14:40:23 +0300

    add login, logout, register ... functionalities and home page

--- studyApp/views.py
+++ studyApp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .forms import RegistrationForm
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+class RegistrationView(SuccessMessageMixin, FormView):
+    template_name = 'register.html'
+    form_class = RegistrationForm
+    success_url = reverse_lazy('studyApplogin')
+    success_message = "Account for %(username)s created successfully"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home.html')
--- studyHub/settings.py
+++ studyHub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'studyHub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'studyApp' / 'templates' / 'studyApp'],  # Add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,8 +69,18 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'studyHub.wsgi.application'
 
+
+LOGIN_REDIRECT_URL = 'studyApp:home'
+
+STATIC_URL = '/static/'
+
+# Define the directories where Django will search for static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'studyApp/static'),
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -123,3 +135,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
