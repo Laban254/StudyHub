f1928e500e036e79936e91a53d11596b83043d02
Laban Kibet <64686919+Laban254@users.noreply.github.com>
2024-05-16 12:22:33 +0300

    Update settings.py

--- studyHub/settings.py
+++ studyHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l#h03mma*q*^ck7uq1of*fewb4d+*ee!5!p$@0cibl^zdvi4rv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studyhub-8yqw.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
